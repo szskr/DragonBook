d1bba7b0e0572283dac372651e507e0fb3c0cf29
szskr <seizosakurai@mac.com>
2020-02-09 11:27:24 +0900

    IMPRO

--- PconAlgDs/alds1_2_d.c
+++ PconAlgDs/alds1_2_d.c
@@ -12,7 +12,28 @@ struct goo {
 
 #define MAX 1000000
 long long cnt;
-	  
+
+int
+compute_goo(int **g, int n)
+{
+  int size;
+  int i;
+
+  /*
+   * g(n) = 3*g(n-1) + 1 ==> g(n) = 3*(3**(n-1) - 1)/2 + 1
+   */
+  size = (int) (log10((double)(2*n+1))/LOG10_3);
+  *g = (int *) calloc(size+1, sizeof (int));
+  if (*g == 0)
+    return (0);
+  
+  (*g)[0] = 1;
+  for (i = 1; i < size; i++)
+    (*g)[i] = 3*(*g)[i-1] + 1;
+
+  return (size+1);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -25,14 +46,7 @@ main(int argc, char *argv[])
   for (i = 0; i < n; i++)
     scanf("%d", &a[i]);
 
-  /*
-   * g(n) = 3*g(n-1) + 1 ==> g(n) = 3*(3**(n-1) - 1)/2 + 1
-   */
-  g.size = (int) (log10((double)(n+1))/LOG10_3);
-  g.g = (int *) calloc(g.size+1, sizeof (int));
-  g.g[0] = 1;
-  for (i = 1; i < g.size; i++)
-      g.g[i] = 3*g.g[i-1] + 1;
+  g.size = compute_goo(&(g.g), n);
   
   printf("n = %d, g.size = %d\n", n, g.size);
   for (i = 0; i < g.size; i++)
