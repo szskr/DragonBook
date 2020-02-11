54d76ff5dd6fc3528f041ba710e162cbd1395c17
szskr <seizosakurai@mac.com>
2020-02-11 09:45:36 +0900

    IMPRO

--- PconAlgDs/alds1_3_a.c
+++ PconAlgDs/alds1_3_a.c
@@ -16,16 +16,30 @@ struct stack {
   int status;
 };
 
-int *
-initialize(Stack *s, int size)
+Stack *
+alloc_stack(int size)
 {
-  s->s = calloc(size, sizeof(int));
-  if (s->s == NULL)
-    return ((int *) ERROR);
+  Stack *s;
+
+  s = (Stack *) calloc(1, sizeof (Stack));
+  if (s == (Stack *)NULL)
+    return ((Stack *) NULL);
+  s->s = calloc(size, sizeof (int));
+  if (s->s == NULL) {
+    free(s);
+    return ((Stack *) NULL);
+  }
   s->size = size;
-  s->top = 0;
   s->status = S_OK;
-  return (s->s);
+  s->top = 0;
+  return (s);
+}
+
+void
+free_stack(Stack *s)
+{
+  free(s->s);
+  free(s);
 }
 
 int
@@ -71,32 +85,37 @@ int
 main(int argc, char *argv[])
 {
   int a, b, c;
-  Stack s;
+  Stack *s;
   char str[MAX];
 
-  initialize(&s, MAX);
+  s = alloc_stack(MAX);
+  if (s == (Stack *) ERROR) {
+    fprintf(stderr, "could not allocate memory\n");
+    exit(1);
+  }
 
   while (scanf("%s", str) != EOF) {
     switch (str[0]) {
     case '+':
     case '-':
     case '*':
-      a = pop(&s);
-      b = pop(&s);
+      a = pop(s);
+      b = pop(s);
       if (str[0] == '+')
 	c = a + b;
       else if (str[0] == '-')
 	c = a - b;
       else
 	c = a * b;
-      push(&s, c);
+      push(s, c);
       break;
     default:
-      push(&s, atoi(str));
+      push(s, atoi(str));
       break;
     }
   }
-  printf("%d\n", pop(&s));
+  printf("%d\n", pop(s));
+  free_stack(s);
    
   printf("\n");
 }
