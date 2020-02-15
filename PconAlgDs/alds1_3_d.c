0faf511672d1b1ec3cb4b5cce15bfb0e0c9b098d
szskr <seizosakurai@mac.com>
2020-02-15 10:07:10 +0900

    IMPRO

---
+++ PconAlgDs/alds1_3_d.c
@@ -0,0 +1,11 @@
+/*
+ * Area s on the Cross-Section Diagram
+ */
+#include "pcon.h"
+
+
+int
+main(int argc, char *argv[])
+{
+  return (0);
+}
---
+++ PconAlgDs/alds1_4_a.c
@@ -0,0 +1,10 @@
+/*
+ * Linear Search
+ */
+#include "./pcon.h"
+
+int
+main(int argc, char *argv[])
+{ 
+  printf("\n");
+}
