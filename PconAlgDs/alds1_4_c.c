07ffcf76c98d87d79ce6dc94572a258042a0a559
szskr <seizosakurai@mac.com>
2020-02-19 22:25:34 +0900

    IMPRO

--- PconAlgDs/alds1_4_c.c
+++ PconAlgDs/alds1_4_c.c
@@ -1,5 +1,5 @@
 /*
- * Binary Search
+ * Hash
  */
 #include "./pcon.h"
 
--- PconAlgDs/pcon.h
+++ PconAlgDs/pcon.h
@@ -11,6 +11,8 @@
 #define TRUE  1
 #define FALSE 0
 
+typedef long long llong;
+
 #define LOG10_3 0.4771
 
 #define swap(type, x, y) {type _t_; _t_ = x; x = y; y = _t_;}
