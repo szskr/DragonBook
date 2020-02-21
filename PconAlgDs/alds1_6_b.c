49e625188b7ab48dff28d3af45115164588a7f96
szskr <seizosakurai@mac.com>
2020-02-21 16:19:54 +0900

    IMPRO

---
+++ PconAlgDs/alds1_6_b.c
@@ -0,0 +1,57 @@
+/*
+ * Merge Sort
+ */
+#include "./pcon.h"
+
+int
+partition(int *a, int p, int r)
+{
+  int j;
+  int x = a[r];
+  int i = p - 1;
+
+  printf("a[%d] = %d\n", r, a[r]);
+  
+  for (j = p; j < r; j++) {
+    if (a[j] <= x) {
+      i++;
+      swap(int, a[i], a[j]);
+    }
+  }
+
+  swap(int, a[i+1], a[r]);
+  
+  return (i+1);
+}
+
+int
+main(int argc, char *argv[])
+{
+  int n;
+  int *a;
+  int i;
+  int q;
+
+  scanf("%d", &n);
+  
+  a = (int *) malloc(n * sizeof (int));
+  if (a == (int *) NULL)
+    return (1);
+  
+  for (i = 0; i < n; i++)
+    scanf("%d", a+i);
+
+  q = partition(a, 0, n-1);
+
+  for (i = 0; i < n; i++) {
+    if (i)
+      printf(" ");
+    if (i == q)
+      printf("[");
+    printf("%d", a[i]);
+    if (i == q)
+      printf("]");
+  }
+  printf("\n");
+  return (0);
+}
