8ec12f2830ce9fb24e456e0df4346557aaec494c
szskr <seizosakurai@mac.com>
2020-02-28 00:24:08 +0900

    IMPRO

--- PconAlgDs/alds1_7_a.c
+++ PconAlgDs/alds1_7_a.c
@@ -3,40 +3,57 @@
  */
 #include "./pcon.h"
 
-#define MAX 10
-#define MAX_CHILDREN 10
-
 typedef struct node Node;
-struct node { /* left-child, right sibling representation */
+struct node {         /* left-child, right sibling representation */
   Node *parent;
-  Node *child_lm;  /* left most child */
-  Node *sibling_rm;  /* right most sibling */
+  Node *child_lm;     /* left most child */
+  Node *sibling_imr;  /* sibling immidiately right */
   int id;
 };
 
-int
-is_root(Node *n)
-{
-  return (n == (Node *) NULL);
-}
 
 int
-is_leaf(Node *n)
+depth(Node *n)
 {
-  return (n->child_lm == (Node *) 0);
-}
+  int d = 0;
 
-int
-is_internal_node(Node *n)
-{
-  return ((n->parent != n) &&
-	  (n->child_lm != 0));
+  while (n->parent != n) {
+    d++;
+    n = n->parent;
+  };
+  return (d);
 }
 
 void
 dump_info(Node *nodes, int i)
 {
-  printf ("Node(%2d) id:%2d : parent %lu\n", i, nodes[i].id, (&nodes[0] - nodes[i].parent)/sizeof (Node));
+  Node *n = nodes + i;
+  char *type;
+  int id;
+
+  if (n->parent == n)
+    type = "root";
+  else if (n->child_lm != (Node *) NULL)
+    type = "node";
+  else
+    type = "leaf";
+
+   printf ("Node :%2d, parent:%2lu, dep:%d, %s, [",
+	  i,
+	  (n->parent - nodes),
+	  depth(n),
+	  type);
+
+  if (n->child_lm != (Node *) NULL) {
+    Node *c = n->child_lm;
+    do {
+      printf("%d,", c - nodes);
+      c = c->sibling_imr;
+    } while (c != c->sibling_imr);
+    printf("%d", c - nodes);
+   }
+  printf("]");
+  printf("\n");
 }
 
 int
@@ -44,7 +61,6 @@ main(int argc, char *argv[])
 {
   int n, i, j;
   Node *nodes;
-  Node **nodeps;
   
   scanf("%d", &n);
   if (n <= 0) {
@@ -59,41 +75,39 @@ main(int argc, char *argv[])
   };
 
   for (i = 0; i < n; i++)
-    nodes[i].parent = &nodes[i];
+    (nodes+i)->parent = nodes + i;
   
-  nodeps = (Node **) malloc((n-1) * (sizeof (Node *)));
-  if (nodeps == (Node **) NULL) {
-    fprintf(stderr, "malloc(): error\n");
-    exit(1);
-  }
- 
   for (i = 0; i < n; i++) {
     int id;
     int num_child;
     int child;
-
+    Node *nchild;
+    Node *pchild;
+    Node *np = nodes + i;
+    
+    np->child_lm = (Node *) NULL;
+    pchild = (Node *) NULL;
+ 
     scanf("%d", &id);
-    nodes[i].id = id;
 
+    /*
+     * Get the number of children
+     */
     scanf("%d", &num_child);
-    printf("HANDLING (%d): num_child = %d\n", id, num_child);
     for (j = 0; j < num_child; j++) {
       scanf("%d", &child);
-      printf("  Child = %d Parent = %d\n", child, id);
-      nodes[child].parent = &nodes[id];
-      nodeps[j] = &nodes[child];
-    }
-
-    if (num_child == 0) {
-      nodes[i].child_lm = (Node *) NULL;
-       } else {
-       for (j = 0; j < num_child; j++) {
-	nodeps[j]->sibling_rm = &nodes[num_child-1];
-      }
-      nodes[i].child_lm = nodeps[0];
-    }
+      nchild = nodes + child;
+      if (j == 0)
+	np->child_lm = nchild;
+      nchild->parent = np;
+      if (pchild != (Node *) NULL)
+	pchild->sibling_imr = nchild;
+      nchild->sibling_imr = nchild;
+      pchild = nchild;
+     }
    }
 
+  printf("\n");
   for (i = 0; i < n; i++)
     dump_info(nodes, i);
   return (0);
